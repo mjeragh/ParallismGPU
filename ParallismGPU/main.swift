308650d3709dd1286f4ddad8171f9108ee7c8385
Mohammad Jeragh <mjeragh@gmail.com>
2019-10-05 11:43:21 +0300

    Initial Commit

---
+++ ParallismGPU/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ParallismGPU
+//
+//  Created by Mohammad Jeragh on 10/5/19.
+//  Copyright © 2019 Mohammad Jeragh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
